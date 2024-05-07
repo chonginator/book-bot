555e334f2b3a09f96756d88276d9bf73536a7ef7
chonginator <benchong1999@yahoo.com.au>
2024-05-07 12:32:18 +1000

    add count words function

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ def get_book_text(path):
   with open(path) as f:
     return f.read()
 
+def count_words(text):
+  words = text.split()
+  return len(words)
+
 def main():
   book_path = "books/frankenstein.txt"
   text = get_book_text(book_path)
-  print(text)
+  print(count_words(text))
 
 main()
